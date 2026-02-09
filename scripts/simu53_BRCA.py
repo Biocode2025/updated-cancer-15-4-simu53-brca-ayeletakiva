b5f4e1d13e6a53ba934624394c9e84de2b45f11f
ayeletakiva <ayelet.akiva@gmail.com>
2026-02-09 16:54:06 +0200

    הוספתי פונקציה המקבלת קובץ דנ"א והופכת אותו לרצף רנ"א

--- scripts/simu53_BRCA.py
+++ scripts/simu53_BRCA.py
@@ -1,4 +1,15 @@
-
+#פונקציה המקבלת קובץ דנ"א והופכת אותו לרצף רנ"א
+def DNA_RNA_Cod(DNA):
+  delimiter = ""
+  line_list = []
+  DNA = DNA.upper()
+  for ch in DNA:
+    if ch == "T":
+      line_list.append("U")
+    else:
+      line_list.append(ch)
+  RNA = delimiter.join(line_list)
+  return RNA
 ### main program ###
 
 gene_occ = input("Does the Female has a BRCA1,2 mutation? (Y=Yes, N=No)")
