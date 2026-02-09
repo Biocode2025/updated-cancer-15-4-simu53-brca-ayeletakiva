2dfec38caeb4fe4e0bdd05d1a6200a106271c757
ayeletakiva <ayelet.akiva@gmail.com>
2026-02-09 16:52:11 +0200

    תחילת התוכנית הראשית

---
+++ scripts/simu53_BRCA.py
@@ -0,0 +1,8 @@
+
+### main program ###
+
+gene_occ = input("Does the Female has a BRCA1,2 mutation? (Y=Yes, N=No)")
+
+if gene_occ == "yes":
+    print("For a female that does have BRCA1,2 Mutation: \nThe mutation that will change the P53 protein will take in average XX years.")
+    
